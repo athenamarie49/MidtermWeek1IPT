670ab74b3d88bbd7a0778800adcd502503e388dc
athenamarie49 <athenamariee49@gmail.com>
2021-10-05 00:11:42 +0800

    2nd committ

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,8 +4,8 @@
 
     <div class="jumbotron jumbotron-fluid">
         <div class="container">
-        <h1 class="display-4 mt-5">Midterm Week1</h1>
-        <p class="lead"> CRUD</p>
+        <h1 class="display-4 mt-5">Hello! Glad you're here!</h1>
+        <p class="lead"> This is my project!</p>
         </div>
     </div>
 
